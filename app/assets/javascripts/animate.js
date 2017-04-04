059482fdc3194079549efe216cbae92d5b2e3733
Basia Ciochon <barbara.ciochon@gmail.com>
2017-04-04 15:34:02 -0700

    Minor adjustments to margins

---
+++ app/assets/javascripts/animate.js
@@ -0,0 +1,7 @@
+// function Qs(query) { return document.querySelectorAll(query)}
+//
+// const hoverItem = Qs('.m-item');
+//
+// hoverItem.addEventListener('mouseenter', function (event) {
+//   const target = event.target
+// })
